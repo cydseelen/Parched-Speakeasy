4de6348b8474a3bd20546489242257461e1512a4
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-01-16 16:20:19 +0100

    should destroy child doses when destroying self.

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
-  has_many :doses
-  has_many :ingredients, dependent: :destroy
+  has_many :doses, dependent: :destroy
+  has_many :ingredients
 
   validates :name, uniqueness: { case_sensitive: false }
   validates :name, presence: true
