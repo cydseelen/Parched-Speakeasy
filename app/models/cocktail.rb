682dd256808d09a94e6eca3df34bea1a09d519d3
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-01-16 16:23:54 +0100

    2 more.

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
-  has_many :ingredients
+  # has_many :ingredients
 
   validates :name, uniqueness: { case_sensitive: false }
   validates :name, presence: true
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,6 @@
 class Ingredient < ApplicationRecord
+  has_many :doses
+
   validates :name, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
 end
